b8841d5460ebf72da56e77ef00b7719985346950
Clayton Wallace <cwallace@academic.rrc.ca>
2013-11-04 13:20:03 -0600

    Initial commit for twitter challenge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterChallenge::Application.config.secret_token = 'fb504bb62a163c6d42ae16c8083cc43ca4399e5e3738715af72a32ee39aaf2a3927d19934dfec3a7125c99e8ca6187daa824426c233a1f9855c6a7681857bf52'
